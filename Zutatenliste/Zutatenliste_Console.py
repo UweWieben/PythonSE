2e7cc8be8971089f72b6159101542bd8f14249af
Uwe Wieben <mail@uwe-wieben.de>
2026-07-14 07:22:22 +0200

    Mit Leerzeichen ausgerichtet

--- Zutatenliste/Zutatenliste_Console.py
+++ Zutatenliste/Zutatenliste_Console.py
@@ -1,9 +1,11 @@
 """
 Gibt eine Zutatenliste für ein Rezept in der Konsole aus.
+- Spaltenbreiten mit Hilfe von Leerzeichen ausgerichtet 
+- Die jeweilige Menge wird rechtsbündig ausgerichtet, die Einheit und die Zutat werden linksbündig ausgerichtet.
 """
 
-print("100 gr Käse") 
-print("200 ml Milch")
-print("50 gr Mehl")
-print("30 gr Butter")
-print("1 Prise Salz")
+print("100 gr    Käse") 
+print("200 ml    Milch")
+print(" 50 gr    Mehl")
+print(" 30 gr    Butter")
+print("  1 Prise Salz")
